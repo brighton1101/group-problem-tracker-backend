4ac75d97798175c89bfd9c6ad844bd3f6aaffeef
Brighton Balfrey <balfrey@usc.edu>
2020-07-15 21:24:13 -0700

    Added base parser class functionality.

---
+++ src/test/java/com/pm/backend/util/parser/ParserTest.java
@@ -0,0 +1,29 @@
+package com.pm.backend.util.parser;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import java.util.Optional;
+
+/**
+ * Tests for com.pm.backend.util.parser.Parser
+ * Note: Later, add tests for fetching doc from url
+ * with Mockito...
+ */
+class ParserTest {
+
+	// Note that this method is highly coupled to `loadFromFile`
+	// currently, and that method is only there for testing purposes
+	// to begin with...
+	@Test
+	public void testGetTextFromElement() {
+		Document doc = Parser
+			.loadFromFile("helloworld.html")
+			.get();
+		Assertions.assertEquals(
+			"Load a Document from a File",
+			Parser.getTextFromElement(doc, ".THIS-IS-FOR-TEST").get()
+		);
+	}
+}
