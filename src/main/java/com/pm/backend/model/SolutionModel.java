314e2b440a43f180b6a8eaa443e55b510e14bdbf
Josh Tai <jitai@ucsc.edu>
2020-07-18 00:34:28 -0700

    crud done for questions

---
+++ src/main/java/com/pm/backend/model/SolutionModel.java
@@ -0,0 +1,9 @@
+package com.pm.backend.model;
+
+public class SolutionModel {
+
+    //referenced to user
+    //referenced to group
+    private String solutionCode;
+    private String solutionLanguage;
+}
