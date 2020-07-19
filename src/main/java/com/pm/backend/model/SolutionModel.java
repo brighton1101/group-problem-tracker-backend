3e554a7f40387e86b8deff836191c8233dffaad7
Josh Tai <jitai@ucsc.edu>
2020-07-18 18:52:24 -0700

    solution model done

--- src/main/java/com/pm/backend/model/SolutionModel.java
+++
@@ -1,9 +0,0 @@
-package com.pm.backend.model;
-
-public class SolutionModel {
-
-    //referenced to user
-    //referenced to group
-    private String solutionCode;
-    private String solutionLanguage;
-}
--- src/main/java/com/pm/backend/model/v1/question/QuestionModel.java
+++ src/main/java/com/pm/backend/model/v1/question/QuestionModel.java
@@ -1,18 +1,19 @@
 package com.pm.backend.model.v1.question;
 
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.experimental.Accessors;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import static com.pm.backend.model.v1.SchemaVersion.v;
 
 import java.util.List;
 
 @Data
 
-@Document(collection = "questions")
-@Accessors(chain = true)
+@Document(collection= "questions")
 public class QuestionModel {
     @Id
     private int id;
@@ -21,7 +22,8 @@ public class QuestionModel {
     private QuestionDifficulty questionDifficulty;
     private String questionDescription;
     private List<String> questionTopics;
+    private final int schemaVersion = v;
 
-    @ApiModelProperty(hidden = true)
-    private int schemaVersion;
+
+
 }
