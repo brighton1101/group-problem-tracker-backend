43186853667e2013e0ee33884cf170d3948be292
Brighton Balfrey <balfrey@usc.edu>
2020-12-24 18:47:20 -0800

    Revert "Keycloak basic user register/login functionality (#7)" (#9)
    This reverts commit 81a98882c95558a1c5ec865e2d633ae7fb8fda96.

--- docker/keycloak/export.sh
+++ docker/keycloak/export.sh
@@ -4,4 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 docker exec -it keycloak /opt/jboss/keycloak/bin/standalone.sh \
 -Djboss.socket.binding.port-offset=100 -Dkeycloak.migration.action=export \
 -Dkeycloak.migration.provider=singleFile \
+-Dkeycloak.migration.realmName=ProblemTracker \
+-Dkeycloak.migration.usersExportStrategy=REALM_FILE \
 -Dkeycloak.migration.file=/tmp/ProblemTracker.json
